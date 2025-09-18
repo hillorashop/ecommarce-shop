f8b8c9ae81f26ce2ef523f5f9ffc2deff2bd9cd8
sajib <sahasojib0155@gmail.com>
2025-09-18 17:34:00 +0600

    fdsf

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export const metadata: Metadata = {
     template: `%s | ${siteMeta.siteName} - পাহাড়ি ঐতিহ্যের ই-কমার্স`,
   },
   icons: [
-    { rel: "icon", url: "/favicon.ico" },
+    { rel: "icon", url: "/logo.svg" },
     { rel: "apple-touch-icon", url: "/logo.svg", sizes: "180x180" },
   ],
   description: siteMeta.desc,

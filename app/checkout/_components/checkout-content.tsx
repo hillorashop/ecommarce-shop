6cea3763664c7460db4d53719f887bd7bc0ee4b2
sajib <sahasojib0155@gmail.com>
2025-11-04 14:23:34 +0600

    fdsfd

--- app/checkout/_components/checkout-content.tsx
+++ app/checkout/_components/checkout-content.tsx
@@ -83,7 +83,7 @@ export const CheckoutContent = ({ productId }: Props) => {
         discount: item.price - price,
         index,
         item_brand: siteMeta.siteName,
-        item_category: item.productId,
+        item_category: " ",
         price,
         quantity: item.cartQuantity,
       };

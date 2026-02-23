e90c61db08adc5b2183c3f26fa72b78a415c6be2
sajib <sahasojib0155@gmail.com>
2026-02-23 21:58:27 +0600

    fsaf

--- components/footer/mobile-footer-navbar.tsx
+++ components/footer/mobile-footer-navbar.tsx
@@ -1,18 +1,17 @@
 "use client";
 
 import { useUser } from "@/contexts/UserContext";
-import {Home, ShoppingBasket, ShoppingCart, Truck, User } from "lucide-react";
+import {Home, ShoppingBasket, Truck, User } from "lucide-react";
 import Link from "next/link";
 import { BiCategory } from "react-icons/bi"
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
-import { useCart, useOpenStore } from "@/hooks/use-store";
-import { Badge } from "../ui/badge";
+
 
 export const navLinks = [
-  { Icon: BiCategory, href: "/categories" },
-{ Icon: ShoppingBasket, href: "/products" },
- { Icon: Home, href: "/" },
-  { Icon: Truck, href: "/tracking" },
+  { Icon: BiCategory, href: "/categories", label:"Categories" },
+{ Icon: ShoppingBasket, href: "/products", label:"Products" },
+ { Icon: Home, href: "/" , label:"Home"},
+  { Icon: Truck, href: "/tracking", label:"Tracking" },
  
 ];
 
@@ -23,9 +22,10 @@ export const MobileFooterNavbar = () => {
         <div className="fixed bottom-0 left-0 right-0 z-50 bg-primary backdrop-blur-lg shadow-sm lg:hidden">
             <div className="px-6 ">
                 <div className="h-14 relative flex justify-between items-center">
-                {navLinks.map(({href, Icon}, i)=> (
-                    <Link href={href} key={i}>
+                {navLinks.map(({href, Icon, label}, i)=> (
+                    <Link href={href} key={i} className="flex flex-col space-y-1 items-center">
                         <Icon className="size-6 text-white"/>
+                        <p className="text-sm font-semibold text-white text-shadow-2xs">{label}</p>
                     </Link>
                 ))}
 

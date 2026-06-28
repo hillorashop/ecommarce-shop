86e05302f82d4b529ad6e7fda0ef8f127abe4b6c
Freefeelbd <sha2dai2kas5@gmail.com>
2026-06-28 19:43:07 +0600

    fsdF

--- app/products/_components/relatedProducts.tsx
+++ app/products/_components/relatedProducts.tsx
@@ -9,7 +9,7 @@ export default function RelatedProducts({ products }: RelatedProductsProps) {
   return (
     <div className="mt-12">
       <h2 className="text-xl font-semibold mb-6">Related Items</h2>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {products.slice(0,8).map((product, index) => (
       <ProductCard product={product} key={index} index={index}/>
         ))}

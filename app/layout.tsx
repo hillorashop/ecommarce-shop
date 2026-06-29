e68a3d6cd8b9b49a0ff51118acecfe40d51fbad1
Freefeelbd <sha2dai2kas5@gmail.com>
2026-06-29 15:47:18 +0600

    fSDF

--- app/layout.tsx
+++ app/layout.tsx
@@ -125,7 +125,7 @@ export default function RootLayout({
           <UserProvider>
             <main className="max-w-[120rem] mx-auto">
               <Navbar />
-              <div className="mt-14 lg:mt-30">
+              <div className="mt-18 lg:mt-36">
                 {children}
                 <Cart />
               </div>
